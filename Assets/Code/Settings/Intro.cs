73782e1477076c312887ba981ca9887c56a0776d
Agustínzero1 <Agustinzero1@gmail.com>
2026-02-01 19:49:39 -0300

    Buildeo de la presentacion

---
+++ Assets/Code/Settings/Intro.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    public void StartLVLTuto()
+    {
+        SceneManager.LoadScene("TutorialMono");
+    }
+    
+}
